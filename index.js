e1eb865b54d4cac14d5423a6fdf8a694a9585019
sean (costco asus vivobook) <ussa.say@gmail.com>
2026-08-20 21:01:10 -0700

    big OOP
    Co-authored-by: Sean Perry <Sean1572@users.noreply.github.com>
    Co-authored-by: Nolan Chai <NolanChai@users.noreply.github.com>
    Co-authored-by: Nick <nick-ls@users.noreply.github.com>

--- index.js
+++ index.js
@@ -5,32 +5,75 @@ canvas.height = window.innerHeight;
 
 const c = canvas.getContext("2d");
 
+// bro is thsi AI???
+// https://stackoverflow.com/questions/1484506/random-color-generator
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+const arena = {
+  width: canvas.width,
+  height: canvas.height,
+};
+
+const player = {
+  x: 100,
+  y: 100,
+  width: 32,
+  height: 32,
+  color: getRandomColor(),
+};
+
 document.addEventListener("resize", () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 });
-
-c.fillStyle = "red";
-c.fillRect(0, 0, window.innerWidth, window.innerHeight);
 const sheepImage = await createImageBitmap(
   await fetch("./assets/sheep.png").then((r) => r.blob()),
 );
-c.drawImage(sheepImage, 0, 0);
 
-c.fillStyle = 'black'
-c.fillText("fuck you", 50, 50);
+/**
+ * I'm the renderer
+ * I'm the one who renders
+ * This function runs once every ~20ms but it can run as fast
+ * as you want it to! It should be fine to call this as often
+ * as you want and the gameplay should be unaffected.
+ *
+ * This is
+ */
+(async function render() {
+  c.fillStyle = "rgba(255, 0, 0, 0.01)";
+  c.fillRect(0, 0, window.innerWidth, window.innerHeight);
 
-class MovingObject {
-  constructor() {}
+  c.drawImage(sheepImage, 0, 0);
+  c.fillStyle = "black";
+  c.fillText("fuck you", 50, 50);
 
-  movement() {}
-}
+  requestAnimationFrame(render);
+})();
 
-let i = 0
+(async function physics() {
+  //
+})();
+
+let x = 0;
+let y = 0;
 while (true) {
-c.fillText("FUCK", 50 + i * 20, 100);
-i++
+  c.fillText("FUCK", 50 + x * 20, 100 + y * 20);
+  x++;
+  if (x > 30) {
+    x = 0;
+    y++;
+  }
+  if (y > 30) {
+    y = 0;
+  }
 
   // wait for one frame
-  await new Promise(resolve => window.requestAnimationFrame(resolve))
+  await new Promise((resolve) => window.requestAnimationFrame(resolve));
 }
