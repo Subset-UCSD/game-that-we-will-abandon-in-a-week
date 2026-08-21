c23ec720e2f42b1b28818fce17a3b6d0ae67e936
sean (costco asus vivobook) <ussa.say@gmail.com>
2026-08-20 20:55:20 -0700

    ???
    Co-authored-by: Sean Perry <Sean1572@users.noreply.github.com>
    Co-authored-by: Nick <nick-ls@users.noreply.github.com>
    Co-authored-by: Nolan Chai <NolanChai@users.noreply.github.com>

--- index.js
+++ index.js
@@ -6,27 +6,31 @@ canvas.height = window.innerHeight;
 const c = canvas.getContext("2d");
 
 document.addEventListener("resize", () => {
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-})
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
 
 c.fillStyle = "red";
 c.fillRect(0, 0, window.innerWidth, window.innerHeight);
-const image = await createImageBitmap(await fetch('./assets/sheep.png').then(r => r.blob()))
-c.drawImage(image, 0, 0)
+const sheepImage = await createImageBitmap(
+  await fetch("./assets/sheep.png").then((r) => r.blob()),
+);
+c.drawImage(sheepImage, 0, 0);
 
-
+c.fillStyle = 'black'
 c.fillText("fuck you", 50, 50);
 
-
-
 class MovingObject {
-    constructor() {
-    }
-    
-    movement() {
-        
-    }
+  constructor() {}
+
+  movement() {}
 }
 
-// while 
+let i = 0
+while (true) {
+c.fillText("FUCK", 50 + i * 20, 100);
+i++
+
+  // wait for one frame
+  await new Promise(resolve => window.requestAnimationFrame(resolve))
+}
