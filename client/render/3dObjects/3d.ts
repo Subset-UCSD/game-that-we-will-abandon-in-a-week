7de94055fea1901eca83933fa7f9a3b8c786d90c
Sean1572 <sean.hyatt.perry@gmail.com>
2026-08-23 23:20:50 -0700

    add texture

--- client/render/3dObjects/3d.ts
+++ client/render/3dObjects/3d.ts
@@ -7,6 +7,9 @@ import { Canvas } from "../canvas";
 import { RenderableObject } from "../render";
 import { Vec3, vec3, vec3ToDomPoint, DomPointToVec3, cross_product, subVec3 } from "@common";
 
+
+export const identity = new DOMMatrix();
+
 // I can already feel the weight of my sins crawlling on my back for this one for introducting
 // a method to handle 3d shapes in our 2d game engine
 
@@ -17,6 +20,15 @@ interface Renderable3DObject extends RenderableObject {
     //unspoken rule of faces: the vertex at index 0, connects to index 1 and -1
     
 }
+
+class Texture3D {
+    constructor () {
+
+    }
+
+
+}
+
 
 export class Cube implements Renderable3DObject {
     index = 20;
@@ -60,20 +72,30 @@ export class Cube implements Renderable3DObject {
     }
 
 
+
+
     render({c}: Canvas): void {
         const matrix = new DOMMatrix([1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]);
         matrix.rotateSelf(this.rotateX, this.rotateY, this.rotateZ)
         matrix.scaleSelf(100, 100, 100)
-        
+        // this.rotateX--;
+        // this.rotateY++;
+        // this.rotateZ--;
+
+
+        //TEMP
+
         c.beginPath();
         let frist = true
         for (const face of this.faces){
             const face_veterices: Vec3[] = []
             for (const vertexIdx of face) {
                 const vertex = this.vertrices.at(vertexIdx)
+               
                 if (vertex == null) {
                     continue
                 }
+
                 face_veterices.push(DomPointToVec3(matrix.transformPoint(vec3ToDomPoint(vertex))))
             }
 
@@ -88,30 +110,47 @@ export class Cube implements Renderable3DObject {
                 subVec3(face_veterices[2], face_veterices[1]),
                 subVec3(face_veterices[0], face_veterices[1])
             )
+
+           
            
             if (normal.z < 0) {
                 continue
             }
+
+
+            c.save()
+
+            const e = face_veterices[0].x;
+            const f = face_veterices[0].y; 
+            const a = (face_veterices[1].x - e)
+            const b = (face_veterices[1].y - f) ;
+            const c_ = (face_veterices[3].x - e);
+            const d = (face_veterices[3].y - f) ;
+
+            const textureMatrix = new DOMMatrix([ a, b, c_, d, e, f ])
+
+            // c.fillRect("red")
+            c.setTransform(c.getTransform().multiply(textureMatrix));
+            c.fillStyle = "red";
+            c.fillRect(0, 0, 1, 1) //TODO generalize the width heigh
             
-            console.log(face, normal)
+            c.setTransform(identity)
+            c.restore()
+
+            
+            // console.log(face, normal)
 
             for (const drawn_vertex of face_veterices) {
-                // const vertex = this.vertrices.at(vertexIdx)
-                // if (vertex == null) {
-                //     continue
-                // }
-                // const drawn_vertex = DomPointToVec3(matrix.transformPoint(vec3ToDomPoint(vertex)))
-                //console.log(drawn_vertex, "hi?", matrix.transformPoint(vec3ToDomPoint(vertex)))
-
                 if (frist) {
                     frist = false
-                    c.moveTo(drawn_vertex.x, -drawn_vertex.y)
+                    c.moveTo(drawn_vertex.x, drawn_vertex.y)
                 } else {
-                    c.lineTo(drawn_vertex.x, -drawn_vertex.y);  
+                    c.lineTo(drawn_vertex.x, drawn_vertex.y);  
                 }
             }
             c.closePath();
             c.stroke();
+            
             frist = true
            
         }
